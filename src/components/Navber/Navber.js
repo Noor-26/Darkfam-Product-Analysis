adc51a295bdac37eaafe155b4fcf82e5aa8b6468
Noor-26 <nk26982698@gmail.com>
2022-04-04 11:44:32 +0600

    Navber created by custom links

--- src/components/Navber/Navber.js
+++ src/components/Navber/Navber.js
@@ -3,18 +3,32 @@ import CustomLink from '../CustomLink/CustomLink';
 import './Navber.css'
 const Navber = () => {
     return (
-        <nav className="navber container">
-            <div>
-                <h2>Darkfam</h2>
-            </div>
-            <div className="links">
-            <CustomLink to="/">Home</CustomLink>
-            <CustomLink to="/review">Review</CustomLink>
-            <CustomLink to="/charts">Dasahboard</CustomLink>
-            </div>
-            
-
-        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Darkfam</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <CustomLink to="/">Home</CustomLink>
+        </li>
+        <li class="nav-item">
+        <CustomLink to="/review">Review</CustomLink>
+        </li>
+        <li class="nav-item">
+        <CustomLink to="/charts">Dashboard</CustomLink>
+        </li>
+        <li class="nav-item">
+        <CustomLink to="/Blog">Blog</CustomLink>
+        </li>
+        
+      </ul>
+     
+    </div>
+  </div>
+</nav>
     );
 };
 
